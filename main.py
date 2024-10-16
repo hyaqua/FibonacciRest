9f895a58d0e4c67d0379f9ab4dac554e2ad68d7c
Jonas <rudnickasj12@gmail.com>
2024-10-16 21:08:58 +0300

    fix more typos
    Reformatted python file

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template, request
 
+
 def fibonacci(n):
     if n <= 1:
         return 0 if n == 0 else 1
     return fibonacci(n - 1) + fibonacci(n - 2)
 
+
 def create_app():
     app = Flask(__name__)
+
     @app.route('/<num>', methods=['GET'])
     def return_fibonacci_number(num):
         # We check if the passed number is a number, if it is we convert it to an integer
